fff288bb161a5076cafb2ecfd72f66f5422f4990
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-01 14:06:47 +0200

    fixed problem in dummy data

--- src/components/CheckoutItemsList.jsx
+++ src/components/CheckoutItemsList.jsx
@@ -4,20 +4,19 @@ const checkoutItems = {
   16065: {
     description: "Please don't eat this one... for real!",
     id: 16065,
-    images:
-      Array[1][
-        {
-          alt: "an orange painted blue",
-          src: {
-            large:
-              "https://was-reach-hackathon-img.now.sh/mysterious-orange_L.jpg",
-            medium:
-              "https://was-reach-hackathon-img.now.sh/mysterious-orange_M.jpg",
-            small:
-              "https://was-reach-hackathon-img.now.sh/mysterious-orange_S.jpg",
-          },
-        }
-      ],
+    images: [
+      {
+        alt: "an orange painted blue",
+        src: {
+          large:
+            "https://was-reach-hackathon-img.now.sh/mysterious-orange_L.jpg",
+          medium:
+            "https://was-reach-hackathon-img.now.sh/mysterious-orange_M.jpg",
+          small:
+            "https://was-reach-hackathon-img.now.sh/mysterious-orange_S.jpg",
+        },
+      },
+    ],
     name: "Mysterious Orange",
     price: 324,
     rating: 4.7,
@@ -27,20 +26,19 @@ const checkoutItems = {
     description:
       "Get this rare pieace of tech that has been hand painted in a matte white finnish!",
     id: 18272,
-    images:
-      Array[1][
-        {
-          alt: "a white playstation",
-          src: {
-            large:
-              "https://was-reach-hackathon-img.now.sh/white-playstation_L.jpg",
-            medium:
-              "https://was-reach-hackathon-img.now.sh/white-playstation_M.jpg",
-            small:
-              "https://was-reach-hackathon-img.now.sh/white-playstation_S.jpg",
-          },
-        }
-      ],
+    images: [
+      {
+        alt: "a white playstation",
+        src: {
+          large:
+            "https://was-reach-hackathon-img.now.sh/white-playstation_L.jpg",
+          medium:
+            "https://was-reach-hackathon-img.now.sh/white-playstation_M.jpg",
+          small:
+            "https://was-reach-hackathon-img.now.sh/white-playstation_S.jpg",
+        },
+      },
+    ],
     name: "White Playstation",
     price: 4595,
     rating: 4.5,
@@ -50,20 +48,19 @@ const checkoutItems = {
     description:
       "Want to look cool whilst listening to some sweet music? Get theese matte black over-the-ear headphones today!",
     id: 19336,
-    images:
-      Array[1][
-        {
-          alt: "black hadphones",
-          src: {
-            large:
-              "https://was-reach-hackathon-img.now.sh/black-headphones_L.jpg",
-            medium:
-              "https://was-reach-hackathon-img.now.sh/black-headphones_M.jpg",
-            small:
-              "https://was-reach-hackathon-img.now.sh/black-headphones_S.jpg",
-          },
-        }
-      ],
+    images: [
+      {
+        alt: "black hadphones",
+        src: {
+          large:
+            "https://was-reach-hackathon-img.now.sh/black-headphones_L.jpg",
+          medium:
+            "https://was-reach-hackathon-img.now.sh/black-headphones_M.jpg",
+          small:
+            "https://was-reach-hackathon-img.now.sh/black-headphones_S.jpg",
+        },
+      },
+    ],
     name: "Black Headphones",
     price: 3426,
     rating: 3.5,
