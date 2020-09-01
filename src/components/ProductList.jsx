53634faa2fbd0998ea2b1853e3ee789c3a92c6df
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-01 13:37:24 +0200

    mapping products array and returning ProductCard with props

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,38 @@
 import React, { useContext } from "react";
 import { ProductsContext } from "../contexts/GlobalContext";
-//  const ProductsData = useContext(ProductsContext);
-// const { products, reviews, couponCodes, isLoading } = ProductsData;
+import ProductCard from "./ProductCard";
 
 export default function ProductList() {
   const ProductsData = useContext(ProductsContext);
   const { products } = ProductsData;
 
-  console.log(products);
+  products && console.log(Object.entries(products));
 
-  return <div></div>;
+  return (
+    <div>
+      {products &&
+        Object.entries(products).map((product) => {
+          const key = product[1].id;
+          const id = product[1].id;
+          const name = product[1].name;
+          const price = product[1].price;
+          const description = product[1].description;
+          const imgURL = product[1].images[0].src.small;
+          const imgAlt = product[1].images[0].alt;
+          return (
+            <ProductCard
+              key={key}
+              id={id}
+              name={name}
+              price={price}
+              description={description}
+              imgURL={imgURL}
+              imgAlt={imgAlt}
+            ></ProductCard>
+          );
+        })}
+    </div>
+  );
 }
 
 // Loopa ut produkterna
