436a87a3ebc45cbaafc5147c97b433ee6ddb080e
Shan <shan.mi.fanfan@gmail.com>
2020-09-01 23:15:33 +0200

    Refactor
    Refactor some Matilda's code in CheckoutItemsList;
    Added btn to go to checkout page directly (just for quick redirect)

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -8,6 +8,9 @@ const WelcomePage = () => {
       <Link to="/products">
         <button className="btn btn-primary mt-5">Go to products page</button>
       </Link>
+      <Link to="/checkout">
+        <button className="btn btn-info mt-5 ml-3">Go to Checkout page</button>
+      </Link>
     </div>
   );
 };
