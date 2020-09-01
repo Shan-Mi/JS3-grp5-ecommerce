2eb43ff961a0626cf36c6aa103085c6dd3f705b0
Shan <shan.mi.fanfan@gmail.com>
2020-09-01 18:35:05 +0200

    Added btn to go to products page, for now

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const WelcomePage = () => {
-  return <div>Welcome to our E-commerce shop!</div>;
+  return (
+    <div>
+      <h1>Welcome to our E-commerce shop!</h1>
+      <Link to="/products">
+        <button className="btn btn-primary mt-5">Go to products page</button>
+      </Link>
+    </div>
+  );
 };
 
 export default WelcomePage;
