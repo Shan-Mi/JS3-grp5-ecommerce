c6dff992d43809fda5e9a29821a83e8e92be9047
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-01 13:18:51 +0200

    imported productsContext to ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductsContext } from "../contexts/GlobalContext";
+//  const ProductsData = useContext(ProductsContext);
+// const { products, reviews, couponCodes, isLoading } = ProductsData;
 
 export default function ProductList() {
+  const ProductsData = useContext(ProductsContext);
+  const { products } = ProductsData;
+
+  console.log(products);
+
   return <div></div>;
 }
 
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,13 +1,14 @@
 // länka in ProductList
-import React from 'react';
-
+import React from "react";
+import ProductList from "../components/ProductList";
 
 const ProductListPage = () => {
   return (
     <div>
       This is productlistpage
+      <ProductList></ProductList>
     </div>
-  )
-}
+  );
+};
 
-export default ProductListPage
+export default ProductListPage;
