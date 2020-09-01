509f2d5b5f286baab9313f07c3341cbb601c9ae5
Shan <shan.mi.fanfan@gmail.com>
2020-09-01 14:51:03 +0200

    Simple styling for product-detail-info
    TODO:
    1. setCart should check stock status, and find some way to conditional render add-btn (if count >= stock, disable btn)
    2. setCart should set cart item count -> count + 1;
    3. store data in localStorage.

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import WithSpinner from "./withSpinner/WithSpinner";
 import "./ProductInfo.styles.scss";
+import { ProductsContext } from "../contexts/GlobalContext";
 
 const ProductInfo = ({ isLoading, product }) => {
   const {
@@ -13,30 +14,46 @@ const ProductInfo = ({ isLoading, product }) => {
     stock,
   } = product;
 
-  return (
-    <div>
-      {description}
-      <br />
-      {id}
-      <br />
+  const { cart, setCart } = useContext(ProductsContext);
+  // cart: an object that stores all items
+  // setCart: this id-item count +1, check stock status, conditional disable btn.
 
-      <img className="product-detail-img" src={src.medium} alt={alt} />
-      <br />
-      {price}
-      <br />
-      {rating}
-      <br />
-      {name}
-      <br />
-      {stock}
-      <br />
-      {console.log(product)}
-    </div>
+  return (
+    <>
+      <div className="product-detail-container">
+        <div className="product-detail-image">
+          <img className="product-detail-img" src={src.medium} alt={alt} />
+        </div>
+
+        <div className="product-detail-info">
+          <h1>{name}</h1>
+          <h5>
+            <span>Description: </span>
+            {description}
+          </h5>
+          <h5>
+            <span>Price: </span>
+            {price}
+          </h5>
+          <h5>
+            <span>Rating: </span>
+            {rating}
+          </h5>
+          <h5>
+            <span>Stock: </span>
+            {stock}
+          </h5>
+          {/* {id} */}
+          <button onClick={setCart}>Add to Cart</button>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default WithSpinner(ProductInfo);
 
-// Rita upp produkt-info med jsx mha props
-//Detta är detailed view
+/* // Rita upp produkt-info med jsx mha props
+// Detta är detailed view
 // alltså bild, pris mm
+ */
