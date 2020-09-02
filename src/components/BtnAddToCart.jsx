d8c463c54d78ed0ef6f16a3eabf594acf03d9c91
Shan <shan.mi.fanfan@gmail.com>
2020-09-02 10:12:57 +0200

    layout for checkout-form, no functionality

--- src/components/BtnAddToCart.jsx
+++ src/components/BtnAddToCart.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import {useContext} from 'react'
+import { useContext } from "react";
 import { ProductsContext } from "../contexts/GlobalContext";
-import { addItemToCart} from "./utilities";
+import { addItemToCart } from "./utilities";
 
-export default function BtnAddToCart({id}) {
+export default function BtnAddToCart({ id }) {
   const { cart, setCart, products } = useContext(ProductsContext);
   return (
     <button
-            className="btn btn-primary"
-            onClick={() => {
-              setCart(addItemToCart(cart, products[id]),
-              );
-            }}>
-            Add to Cart
-          </button>
+      className="btn btn-primary"
+      onClick={() => {
+        setCart(addItemToCart(cart, products[id]));
+      }}>
+      Add to Cart
+    </button>
   );
 }
 
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CheckoutItemsList from "../components/CheckoutItemsList";
+import CustomerOrderForm from "../components/CustomerOrderForm";
 
 export default function CheckoutPage() {
   return (
     <div>
       This is check out page
       <CheckoutItemsList />
+      <CustomerOrderForm />
     </div>
   );
 }
