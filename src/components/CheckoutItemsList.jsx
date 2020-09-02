24d64c83e939c58665b6b5fab492732119ede0c4
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-09-02 15:12:58 +0200

    deleted old comment

--- src/components/CheckoutItemsList.jsx
+++ src/components/CheckoutItemsList.jsx
@@ -5,7 +5,7 @@ import BtnIncreaseDecrease from "./BtnIncreaseDecrease";
 import BtnDelete from "./BtnDelete";
 import BtnClearCart from "./BtnClearCart";
 
-export default function CheckoutItemsList({discountPrice, setDiscountPrice}) {
+export default function CheckoutItemsList({ discountPrice, setDiscountPrice }) {
   const ProductsData = useContext(ProductsContext); // using dummy data just for now
   const { cart: cartItems } = ProductsData;
   // const cart = checkoutItems;
@@ -16,10 +16,18 @@ export default function CheckoutItemsList({discountPrice, setDiscountPrice}) {
       cartItems &&
       cartItems.map(({ id, name, price, quantity }, index) => (
         <tr key={id}>
-          <td><BtnDelete id={id} /></td>
+          <td>
+            <BtnDelete id={id} />
+          </td>
           <th scope="row">{index + 1}</th>
           <td>{name}</td>
-          <td><BtnIncreaseDecrease quantity={quantity} id={id} setDiscountPrice={setDiscountPrice} /></td>
+          <td>
+            <BtnIncreaseDecrease
+              quantity={quantity}
+              id={id}
+              setDiscountPrice={setDiscountPrice}
+            />
+          </td>
           <td className="text-right">{price * quantity} SEK</td>
         </tr>
       ))
@@ -44,7 +52,9 @@ export default function CheckoutItemsList({discountPrice, setDiscountPrice}) {
       <table className="table table-sm table-hover ">
         <thead className="text-left">
           <tr>
-            <th scope="col"><BtnClearCart /></th>
+            <th scope="col">
+              <BtnClearCart />
+            </th>
             <th scope="col">#</th>
             <th scope="col">Product Name</th>
             <th scope="col">Quantity</th>
@@ -59,7 +69,7 @@ export default function CheckoutItemsList({discountPrice, setDiscountPrice}) {
             <td colSpan="5" className="text-right font-weight-bold">
               {/* Total Price: {cartTotalPrice(cartItems)} SEK */}
               Total Price: {discountPrice} SEK
-              <span className='discount-price d-none'>{discountPrice}</span>
+              <span className="discount-price d-none">{discountPrice}</span>
             </td>
           </tr>
         </tfoot>
@@ -68,72 +78,3 @@ export default function CheckoutItemsList({discountPrice, setDiscountPrice}) {
   );
 }
 // loopa ut alla produkter som finns i cart-context
-
-
-// const checkoutItems = {
-//   16065: {
-//     description: "Please don't eat this one... for real!",
-//     id: 16065,
-//     images: [
-//       {
-//         alt: "an orange painted blue",
-//         src: {
-//           large:
-//             "https://was-reach-hackathon-img.now.sh/mysterious-orange_L.jpg",
-//           medium:
-//             "https://was-reach-hackathon-img.now.sh/mysterious-orange_M.jpg",
-//           small:
-//             "https://was-reach-hackathon-img.now.sh/mysterious-orange_S.jpg",
-//         },
-//       },
-//     ],
-//     name: "Mysterious Orange",
-//     price: 324,
-//     rating: 4.7,
-//     stock: 7,
-//   },
-//   18272: {
-//     description:
-//       "Get this rare pieace of tech that has been hand painted in a matte white finnish!",
-//     id: 18272,
-//     images: [
-//       {
-//         alt: "a white playstation",
-//         src: {
-//           large:
-//             "https://was-reach-hackathon-img.now.sh/white-playstation_L.jpg",
-//           medium:
-//             "https://was-reach-hackathon-img.now.sh/white-playstation_M.jpg",
-//           small:
-//             "https://was-reach-hackathon-img.now.sh/white-playstation_S.jpg",
-//         },
-//       },
-//     ],
-//     name: "White Playstation",
-//     price: 4595,
-//     rating: 4.5,
-//     stock: 2,
-//   },
-//   19336: {
-//     description:
-//       "Want to look cool whilst listening to some sweet music? Get theese matte black over-the-ear headphones today!",
-//     id: 19336,
-//     images: [
-//       {
-//         alt: "black hadphones",
-//         src: {
-//           large:
-//             "https://was-reach-hackathon-img.now.sh/black-headphones_L.jpg",
-//           medium:
-//             "https://was-reach-hackathon-img.now.sh/black-headphones_M.jpg",
-//           small:
-//             "https://was-reach-hackathon-img.now.sh/black-headphones_S.jpg",
-//         },
-//       },
-//     ],
-//     name: "Black Headphones",
-//     price: 3426,
-//     rating: 3.5,
-//     stock: 200,
-//   },
-// };
