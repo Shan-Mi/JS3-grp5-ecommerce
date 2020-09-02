91211aafbdeb03da333ade2d77194fc51bc95bc0
Shan <shan.mi.fanfan@gmail.com>
2020-09-02 11:48:14 +0200

    Checkout stage 1
    1. Check if coupon is valid;
    2. Change price to corresponding discount price if coupon is valid;
    3. Strucuted final data into an object which will be sent to firebase API.

--- src/components/CheckoutItemsList.jsx
+++ src/components/CheckoutItemsList.jsx
@@ -4,7 +4,7 @@ import { cartTotalPrice } from "./utilities";
 import BtnIncreaseDecrease from "./BtnIncreaseDecrease";
 import BtnDelete from "./BtnDelete";
 
-export default function CheckoutItemsList() {
+export default function CheckoutItemsList({discountPrice}) {
   const ProductsData = useContext(ProductsContext); // using dummy data just for now
   const { cart: cartItems } = ProductsData;
   // const cart = checkoutItems;
@@ -56,7 +56,8 @@ export default function CheckoutItemsList() {
         <tfoot>
           <tr>
             <td colSpan="5" className="text-right font-weight-bold">
-              Total Price: {cartTotalPrice(cartItems)} SEK
+              Total Price: {discountPrice} SEK
+              <span className='discount-price d-none'>{discountPrice}</span>
             </td>
           </tr>
         </tfoot>
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import CheckoutItemsList from "../components/CheckoutItemsList";
 import CustomerOrderForm from "../components/CustomerOrderForm";
+import { ProductsContext } from "../contexts/GlobalContext";
+import { cartTotalPrice } from "../components/utilities";
 
 export default function CheckoutPage() {
+  const ProductsData = useContext(ProductsContext); // using dummy data just for now
+  const { cart: cartItems } = ProductsData;
+  const [discountPrice, setDiscountPrice] = useState(cartTotalPrice(cartItems));
   return (
     <div>
       This is check out page
-      <CheckoutItemsList />
-      <CustomerOrderForm />
+      <CheckoutItemsList discountPrice={discountPrice} />
+      <CustomerOrderForm discountPrice={discountPrice} setDiscountPrice={setDiscountPrice} />
     </div>
   );
 }
