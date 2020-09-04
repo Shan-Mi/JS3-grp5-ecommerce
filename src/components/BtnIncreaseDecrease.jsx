ac6c7135af0388b2708e133cdd48d443c719fcf5
Shan <shan.mi.fanfan@gmail.com>
2020-09-04 21:51:10 +0200

    Added increaseBtn status checking & bug fix
    Now if reach stock amount, increase button on checkout page will be disabled;
    Also fixed original price's incorrect displaying problem (once input coupon, original price will not be updated);
    Some styling changes to make sure text and images are placed vertically center.

--- src/components/BtnIncreaseDecrease.jsx
+++ src/components/BtnIncreaseDecrease.jsx
@@ -4,7 +4,7 @@ import { ProductsContext } from "../contexts/GlobalContext";
 import { addItemToCart, removeItemFromCart } from "./utilities";
 import { FaPlus, FaMinus } from "react-icons/fa";
 
-export default function BtnIncreaseDecrease({ quantity, id }) {
+export default function BtnIncreaseDecrease({ quantity, id, isDisabled }) {
   const { cart, setCart, products } = useContext(ProductsContext);
 
   return (
@@ -13,15 +13,18 @@ export default function BtnIncreaseDecrease({ quantity, id }) {
         className="btn"
         onClick={() => {
           setCart(removeItemFromCart(cart, products[id]));
-        }}>
+        }}
+      >
         <FaMinus />
       </button>
       {quantity}
       <button
         className="btn"
+        disabled={isDisabled}
         onClick={() => {
           setCart(addItemToCart(cart, products[id]));
-        }}>
+        }}
+      >
         <FaPlus />
       </button>
     </div>
--- src/components/CheckoutItemsList.jsx
+++ src/components/CheckoutItemsList.jsx
@@ -7,29 +7,38 @@ import BtnClearCart from "./BtnClearCart";
 import { Link } from "react-router-dom";
 
 export default function CheckoutItemsList({ discountPrice, discountRate }) {
-  const ProductsData = useContext(ProductsContext); 
-  const { cart: cartItems } = ProductsData;
+  const ProductsData = useContext(ProductsContext);
+  const { cart: cartItems, products } = ProductsData;
 
   function renderTableRows() {
     return (
       cartItems &&
+      products &&
       cartItems.map(
         ({ id, name, price, quantity, images: [{ alt, src }] }, index) => (
           <tr key={id}>
             <td>
               <BtnDelete id={id} />
             </td>
-            <th scope="row">{index + 1}</th>
-            <th scope="row">
+            <th scope="row" style={{ lineHeight: "38px" }}>
+              {index + 1}
+            </th>
+            <th scope="row" style={{ lineHeight: "38px" }}>
               <img src={src.small} alt={alt} style={imageStyle} />
             </th>
             <td>
-              <Link to={`/products/${id}`}>{name}</Link>
+              <Link style={{ lineHeight: "38px" }} to={`/products/${id}`}>
+                {name}
+              </Link>
             </td>
-            <td>
-              <BtnIncreaseDecrease quantity={quantity} id={id} />
+            <td style={{ lineHeight: "38px" }}>
+              <BtnIncreaseDecrease
+                quantity={quantity}
+                id={id}
+                isDisabled={products[id].stock === quantity}
+              />
             </td>
-            <td className="text-right">
+            <td className="text-right" style={{ lineHeight: "38px" }}>
               {(price * quantity * discountRate).toFixed(2)} SEK
             </td>
           </tr>
@@ -66,7 +75,9 @@ export default function CheckoutItemsList({ discountPrice, discountRate }) {
                 <small>
                   <span className="discount-price mr-5">
                     Original Price:
-                    <del className="font-italic ml-1">{discountPrice} SEK</del>
+                    <del className="font-italic ml-1">
+                      {cartTotalPrice(cartItems)} SEK
+                    </del>
                     <span className="ml-2">
                       Discount: {((1 - discountRate) * 100).toFixed()}%
                     </span>
@@ -88,5 +99,6 @@ export default function CheckoutItemsList({ discountPrice, discountRate }) {
 const imageStyle = {
   float: "left",
   maxHeight: "30px",
+  marginTop: "3px",
   objectFit: "cover",
 };
